3b99d2dce096c9d96d306e133e4b5135de1489a4
jckend <142367196+jckend@users.noreply.github.com>
2025-08-19 17:34:32 -0400

    Update creds-template.ts

--- hosting/src/creds-template.ts
+++ hosting/src/creds-template.ts
@@ -2,13 +2,13 @@ import type { FirebaseOptions } from 'firebase/app'
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const firebaseCred: FirebaseOptions = {
-  apiKey: 'REPLACE-WITH-YOURS',
-  authDomain: 'REPLACE-WITH-YOURS.firebaseapp.com',
-  projectId: 'REPLACE-WITH-YOURS',
-  storageBucket: 'REPLACE-WITH-YOURS.appspot.com',
-  messagingSenderId: 'REPLACE-WITH-YOURS',
-  appId: 'REPLACE:WITH:YOURS',
-  measurementId: 'G-REPLACEWITHYOURS',
+  apiKey: "AIzaSyDor7HRRq5EgIIg2PQI45RGMrVZv6kxjLk",
+  authDomain: "conditional-perfection1.firebaseapp.com",
+  projectId: "conditional-perfection1",
+  storageBucket: "conditional-perfection1.firebasestorage.app",
+  messagingSenderId: "643190366643",
+  appId: "1:643190366643:web:58ba6c46e7290b228cabc6",
+  measurementId: "G-T94BDFYFG7",
 }
 
 export const prolificCompletionCode = 'REPLACE-WITH-YOURS'
